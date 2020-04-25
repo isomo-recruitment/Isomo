380bb679554edf66bc5dffad806c13e800721f0c
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-24 21:38:07 -0400

    completed template

---
+++ .vuepress/components/Hello.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hi this is just a test</h1>
+</template>
---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: "isomo",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "Tab 2", link: "/tab2/" },
+            { text: "Tab 3", link: "/tab3/" },
+            {
+                text: "Github",
+                link: "https://github.com/TheFatPanda97/startup-incubator",
+            },
+        ],
+        sidebar: 'auto',
+    },
+};
