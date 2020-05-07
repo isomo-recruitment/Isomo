58adfd5a2433ed06776b58bf31a029457564dc32
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-05-07 15:20:24 -0400

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,15 +2,13 @@ module.exports = {
     title: "Isomo Recruitment",
     base: "/documentation-website/",
     themeConfig: {
+        logo: "/logo.png",
+        repo: "https://github.com/Isomo-Recruitment",
         nav: [
             { text: "Home", link: "/" },
             { text: "Research", link: "/Research/" },
             { text: "Recruiter", link: "/Recruiter/" },
             { text: "Applicant", link: "/Applicant/" },
-            {
-                text: "Github",
-                link: "https://github.com/Isomo-Recruitment",
-            },
         ],
         sidebar: "auto",
     },
