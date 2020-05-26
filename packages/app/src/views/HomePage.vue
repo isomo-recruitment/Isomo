bafd0f7c8d29c354dd1aea572ea2ff928ae1585c
William Chau <billy.chau@mail.utoronto.ca>
2020-05-26 16:56:07 -0700

    Added divider between about and signup/login

--- packages/app/src/views/HomePage.vue
+++ packages/app/src/views/HomePage.vue
@@ -5,6 +5,7 @@
       <v-btn text color="white" style="font-size:25px">Isomo</v-btn>
       <v-spacer></v-spacer>
       <v-btn text color="white" style="text-transform: capitalize;">About</v-btn>
+      <v-divider dark vertical></v-divider>
       <v-btn text color="white" style="text-transform: capitalize;">Sign Up</v-btn>
       <v-btn text color="white" style="text-transform: capitalize;">Login</v-btn>
     </v-app-bar>
@@ -12,23 +13,23 @@
       <v-parallax src="../assets/homebackground.jpg" height="500">
         <v-row align="center" justify="center">
           <v-col class="text-center" cols="12">
-              <h1 style="font-size:50px; letter-spacing:2px; " >Strive for Equality</h1>
-              <v-btn rounded color="#8be4f0">Join Us</v-btn>
+            <h1 style="font-size:50px; letter-spacing:2px; ">Strive for Equality</h1>
+            <v-btn rounded color="#8be4f0">Join Us</v-btn>
           </v-col>
         </v-row>
       </v-parallax>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
-      <p> test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
     </v-content>
   </v-app>
 </template>
