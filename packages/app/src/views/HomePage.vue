d4b1446a81ec6556e4ba8d637ad5df5fa360f363
William Chau <billy.chau@mail.utoronto.ca>
2020-05-26 16:51:46 -0700

    Added homepage background

--- packages/app/src/views/HomePage.vue
+++ packages/app/src/views/HomePage.vue
@@ -1,35 +1,36 @@
 <template>
-    <v-app>
-        <v-app-bar app color="black" max-height="60" style="opacity: 0.9">
-            <v-img src="../assets/logo.png" max-height="40" max-width="40"></v-img>
-            <v-spacer></v-spacer>
-            <v-btn text color="white" style="text-transform: capitalize;">About</v-btn>
-            <v-btn text color="white" style="text-transform: capitalize;">Sign Up</v-btn>
-            <v-btn text color="white" style="text-transform: capitalize;">Login</v-btn>
-        </v-app-bar>
-        <v-content>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-            <p>Hello this is a test</p>
-        </v-content>
-    </v-app>
+  <v-app>
+    <v-app-bar app color="black" max-height="60" style="opacity: 0.9">
+      <v-img src="../assets/logo.png" max-height="40" max-width="40"></v-img>
+      <v-btn text color="white" style="font-size:25px">Isomo</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text color="white" style="text-transform: capitalize;">About</v-btn>
+      <v-btn text color="white" style="text-transform: capitalize;">Sign Up</v-btn>
+      <v-btn text color="white" style="text-transform: capitalize;">Login</v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-parallax src="../assets/homebackground.jpg" height="500">
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+              <h1 style="font-size:50px; letter-spacing:2px; " >Strive for Equality</h1>
+              <v-btn rounded color="#8be4f0">Join Us</v-btn>
+          </v-col>
+        </v-row>
+      </v-parallax>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+      <p> test</p>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
