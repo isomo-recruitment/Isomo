2e6be5d9c6138f18f88a5e19d18f09e60a8e7d4d
William Chau <billy.chau@mail.utoronto.ca>
2020-05-26 19:02:24 -0700

    Created component for the top bar

---
+++ packages/app/src/component/App-bar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar app color="black" max-height="60" style="opacity: 0.9">
+      <v-img src="../assets/logo.png" max-height="40" max-width="40"></v-img>
+      <v-btn text color="white" style="font-size:25px">Isomo</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text color="white" style="text-transform: capitalize;">About</v-btn>
+      <v-divider dark vertical></v-divider>
+      <v-btn text color="white" style="text-transform: capitalize;">Sign Up</v-btn>
+      <v-btn text color="white" style="text-transform: capitalize;">Login</v-btn>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- packages/app/src/views/HomePage.vue
+++ packages/app/src/views/HomePage.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="black" max-height="60" style="opacity: 0.9">
-      <v-img src="../assets/logo.png" max-height="40" max-width="40"></v-img>
-      <v-btn text color="white" style="font-size:25px">Isomo</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn text color="white" style="text-transform: capitalize;">About</v-btn>
-      <v-divider dark vertical></v-divider>
-      <v-btn text color="white" style="text-transform: capitalize;">Sign Up</v-btn>
-      <v-btn text color="white" style="text-transform: capitalize;">Login</v-btn>
-    </v-app-bar>
+    <app-bar></app-bar>
     <v-content>
       <v-parallax src="../assets/homebackground.jpg" height="500">
         <v-row align="center" justify="center">
@@ -35,5 +27,10 @@
 </template>
 
 <script>
-export default {};
+import AppBar from "../component/App-bar"
+export default {
+    components:{
+        AppBar,
+    }
+};
 </script>
