71b4f02ad2e1896d09ad0ac5ff1a3eaed70b6b9e
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-05-28 21:02:44 -0400

    did some alignment
    Co-authored-by: philemonyen <philemonyen@gmail.com>
    Co-authored-by: williamchau123 <billy.chau@mail.utoronto.ca>
    Co-authored-by: BrianLi009 <brianlizhengyu@gmail.com>

--- packages/app/src/router/index.js
+++ packages/app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomePage from "../views/HomePage.vue";
+import SignUpPage from "../views/SignUpPage.vue"
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
         path: "/",
         name: "HomePage",
         component: HomePage,
+    },
+    {
+        path: "/signup",
+        name: "SignUp",
+        component: SignUpPage
     },
     {
         path: "*",
---
+++ packages/app/src/views/SignUpPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-app>
+        <app-bar></app-bar>
+        <v-content>
+            <v-row align="center">
+                <v-col>
+                    <div style="text-align: center">
+                        <h1>Who are you?</h1>
+                        <div>
+                            <v-btn>Applicant</v-btn>
+                        </div>
+                        <div>
+                            <v-btn>Recruiter</v-btn>
+                        </div>
+                        <div>
+                            <v-btn>Company</v-btn>
+                        </div>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div style="text-align: center">
+                        <v-avatar size="600">
+                            <img src="../assets/signup-background.jpg" />
+                        </v-avatar>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppBar from "../components/AppBar";
+export default {
+    components: {
+        AppBar,
+    },
+};
+</script>
