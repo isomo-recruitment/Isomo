0ff1f652197d394963df5457045a6aeb42e011e2
Shawn Hu <shawnhu97@gmail.com>
2020-05-30 14:04:14 -0400

    finalizing sign up page
    Co-authored-by: philemonyen <philemonyen@gmail.com>
    Co-authored-by: williamchau123 <billy.chau@mail.utoronto.ca>
    Co-authored-by: BrianLi009 <brianlizhengyu@gmail.com>

--- packages/app/src/views/HomePage.vue
+++ packages/app/src/views/HomePage.vue
@@ -1,22 +1,29 @@
 <template>
   <v-app>
     <app-bar></app-bar>
-      <v-parallax src="../assets/homebackground.jpg" height="800">
-        <v-row align="center" justify="center">
-          <v-col class="text-center" cols="12">
-            <h1 style="font-size:70px; letter-spacing:2px; ">Strive for Equality</h1>
-            <v-btn rounded color="#8be4f0">Join Us</v-btn>
-          </v-col>
-        </v-row>
-      </v-parallax>
+    <v-parallax src="../assets/homebackground.jpg" :height="windowHeight">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 style="font-size:70px; letter-spacing:2px; ">
+            Strive for Equality
+          </h1>
+          <v-btn rounded color="#8be4f0" large>Join Us</v-btn>
+        </v-col>
+      </v-row>
+    </v-parallax>
   </v-app>
 </template>
 
 <script>
-import AppBar from "../components/AppBar"
+import AppBar from "../components/AppBar";
 export default {
-    components:{
-        AppBar,
-    }
+  computed: {
+    windowHeight() {
+      return window.innerHeight;
+    },
+  },
+  components: {
+    AppBar,
+  },
 };
 </script>
--- packages/app/src/views/SignUpPage.vue
+++ packages/app/src/views/SignUpPage.vue
@@ -1,39 +1,50 @@
 <template>
-    <v-app>
-        <app-bar></app-bar>
-        <v-content>
-            <v-row align="center">
-                <v-col>
-                    <div style="text-align: center">
-                        <h1>Who are you?</h1>
-                        <div>
-                            <v-btn>Applicant</v-btn>
-                        </div>
-                        <div>
-                            <v-btn>Recruiter</v-btn>
-                        </div>
-                        <div>
-                            <v-btn>Company</v-btn>
-                        </div>
-                    </div>
-                </v-col>
-                <v-col>
-                    <div style="text-align: center">
-                        <v-avatar size="600">
-                            <img src="../assets/signup-background.jpg" />
-                        </v-avatar>
-                    </div>
-                </v-col>
-            </v-row>
-        </v-content>
-    </v-app>
+  <v-app>
+    <app-bar></app-bar>
+    <v-parallax src="../assets/gradient-background.jpg" :height="windowHeight">
+      <v-row align="center">
+        <v-col cols="6">
+          <div style="text-align: center">
+            <h1 style="margin: 30px 0px; font-size: 45px">
+              Who are you?
+            </h1>
+            <div style="margin: 30px 0px">
+              <v-btn width="250" x-large rounded elevation="10" color="#8be4f0"
+                >Applicant</v-btn
+              >
+            </div>
+            <div style="margin: 30px 0px">
+              <v-btn width="250" x-large rounded elevation="10" color="#8be4f0"
+                >Recruiter</v-btn
+              >
+            </div>
+            <div style="margin: 30px 0px">
+              <v-btn width="250" x-large rounded elevation="10" color="#8be4f0"
+                >Company</v-btn
+              >
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="5">
+          <div style="text-align: center">
+            <v-img src="../assets/robot-background.png" contain />
+          </div>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </v-app>
 </template>
 
 <script>
 import AppBar from "../components/AppBar";
 export default {
-    components: {
-        AppBar,
+  computed: {
+    windowHeight() {
+      return window.innerHeight;
     },
+  },
+  components: {
+    AppBar,
+  },
 };
 </script>
