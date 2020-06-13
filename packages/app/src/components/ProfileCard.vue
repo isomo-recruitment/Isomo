9205dfd72e56ed0294383525111177ca393b2028
Shawn Hu <shawnhu97@gmail.com>
2020-06-13 14:24:03 -0400

    Co-authored-by: philemonyen <philemonyen@gmail.com> Co-authored-by: williamchau123 <billy.chau@mail.utoronto.ca> Co-authored-by: BrianLi009 <brianlizhengyu@gmail.com>

--- packages/app/src/components/ProfileCard.vue
+++ packages/app/src/components/ProfileCard.vue
@@ -1,33 +1,57 @@
 <template>
-<v-container fluid>
-  <v-row>
-    <v-card width="1000" height="300">
-      <v-row align=center>
-        <v-col md="auto">
-          <v-icon x-large>mdi-account</v-icon>
-        </v-col>
-        <v-col>
-          <v-card-text>
-            <p class="display-1 text--primary">Username</p>
-          </v-card-text>
-          <v-card-text>
-            <p>User info</p>
-          </v-card-text>
-        </v-col>
-      </v-row>
-      <v-card-text>
-        <p class="text--primary">Area of Expertise</p>
-      </v-card-text>
-    </v-card>
-
-    <v-card width="300">
-      <div align="center">
-        <v-progress-circular :rotate="-90" :size="100" :width="15" :value="75" color="#8be4f0">75</v-progress-circular>
-        <v-card-text></v-card-text>
-      </div>
-    </v-card>
-  </v-row>
-</v-container>
+    <v-container>
+        <v-row>
+            <v-card width="900" height="300">
+                <v-row justify="start" class="ma-0">
+                    <v-card width="350" elevation="0" class="ma-3">
+                        <v-row class="ma-0">
+                            <v-avatar class="ma-3" size="80">
+                                <v-img
+                                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+                                ></v-img>
+                            </v-avatar>
+                            <div>
+                                <v-card-title
+                                    class="headline pb-0"
+                                    style="padding-top: 10px"
+                                >
+                                    Username
+                                </v-card-title>
+                                <v-card-title class="headline-1 pt-0">
+                                    Software Engineer
+                                </v-card-title>
+                                <v-card-subtitle>
+                                    Ontario, CA
+                                </v-card-subtitle>
+                            </div>
+                        </v-row>
+                    </v-card>
+                    <v-spacer></v-spacer>
+                    <v-card width="250" height="300">
+                        <v-row>
+                            <v-col style="text-align:center">
+                                <h3 class="mb-7">Profile Completion</h3>
+                                <v-progress-circular
+                                    :rotate="-90"
+                                    :size="150"
+                                    :width="20"
+                                    :value="75"
+                                    color="#8be4f0"
+                                    class="mb-5"
+                                    style="font-size: 30px"
+                                >
+                                    75
+                                </v-progress-circular>
+                                <h4 class="ma-3" style="color:grey">
+                                    Upload your resume
+                                </h4>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-row>
+            </v-card>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {};
