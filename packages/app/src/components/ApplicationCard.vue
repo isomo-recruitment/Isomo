fba0e0e72a4bcb75f06c07ce838e42963dcad848
williamchau123 <billy.chau@mail.utoronto.ca>
2020-06-27 14:18:25 -0700

    Added blog tab

--- packages/app/src/components/ApplicationCard.vue
+++ packages/app/src/components/ApplicationCard.vue
@@ -1,57 +1,63 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            <v-card width="900" height="420">
-                <v-row class="ma-0 pl-5 pt-1">
-                    <v-col>
-                        <v-tabs>
-                            <v-tab>Applications</v-tab>
-                            <v-tab>Blogs</v-tab>
-                        </v-tabs>
-                    </v-col>
-                    <v-col cols="2" class="mr-2 mt-2">
-                        <v-menu open-on-hover bottom offset-y>
-                            <template v-slot:activator="{ on, attrs }">
-                                <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                                    Dropdown
-                                </v-btn>
-                            </template>
+  <v-container>
+    <v-row justify="center">
+      <v-card width="900" height="420">
+        <v-tabs>
+          <v-tab>Applications</v-tab>
+          <v-tab>Blogs</v-tab>
+          <v-tab-item>
+            <v-row justify="end">
+              <v-col cols="2" class="mr-2 mt-2">
+                <v-menu open-on-hover bottom offset-y>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" dark v-bind="attrs" v-on="on">Dropdown</v-btn>
+                  </template>
 
-                            <v-list>
-                                <v-list-item>
-                                    <v-list-item-title>
-                                        Filter
-                                    </v-list-item-title>
-                                </v-list-item>
-                            </v-list>
-                        </v-menu>
-                    </v-col>
-                </v-row>
-                <v-container>
-                    <v-row justify="center" align="center" dense>
-                        <v-col>
-                            <detail-card status="rejected"></detail-card>
-                        </v-col>
-                        <v-col>
-                            <detail-card status="pending"></detail-card>
-                        </v-col>
-                        <v-col>
-                            <detail-card status="accepted"></detail-card>
-                        </v-col>
-                        <v-col>
-                            <detail-card status="pending"></detail-card>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-card>
-        </v-row>
-    </v-container>
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-title>Filter</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-col>
+            </v-row>
+
+            <v-container>
+              <v-row justify="center" align="center" dense>
+                <v-col>
+                  <detail-card status="rejected"></detail-card>
+                </v-col>
+                <v-col>
+                  <detail-card status="pending"></detail-card>
+                </v-col>
+                <v-col>
+                  <detail-card status="accepted"></detail-card>
+                </v-col>
+                <v-col>
+                  <detail-card status="pending"></detail-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-tab-item>
+          <v-tab-item>
+            <v-row justify="center">
+
+			<blog-post></blog-post>
+			<blog-post></blog-post>
+            </v-row>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import DetailCard from "./DetailCard";
+import BlogPost from "./BlogPost"
 export default {
-    components: {
-        DetailCard,
-    },
+  components: {
+    DetailCard,
+    BlogPost,
+  }
 };
 </script>
---
+++ packages/app/src/components/BlogPost.vue
@@ -0,0 +1,12 @@
+<template>
+<v-row class="ma-2">
+    <v-card width="800"
+            height="110">
+    </v-card>
+</v-row>
+</template>
+<script>
+export default {
+    
+}
+</script>
